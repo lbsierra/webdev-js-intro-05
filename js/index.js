74703df6a3ca06ebb437b929aa9b4d59e1c3b7b6
Sierra <sonicaradia@gmail.com>
2025-01-16 08:39:28 -0500

    Did arrays experiment

--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+let favoriteMovies = ["Sling Blade", "O Brother Where Art Thou", "No Country for Old Men", "The Shawshank Redemption"];
+let favoriteGames = ["Mass Effect", "Skyrim", "Call of Duty MW", "Assassin's Creed", "Arkham Knight"];
+
+favoriteMovies.push("The Dark Knight Rises");
+
+console.log(favoriteMovies);
+
+favoriteGames.pop();
+
+console.log(favoriteGames);
+
+favoriteGames.push("Detroit: Become Human");
+favoriteGames.shift();
+
+console.log(favoriteGames);
+console.log(favoriteMovies[2]);
+
+favoriteGames[3] = "Phoenix Wright: Ace Attorney";
+console.log(favoriteGames);
+
+favoriteMovies[1] = "Reservoir Dogs";
+console.log(favoriteMovies);
+
+console.log(typeof favoriteGames);
+*/
